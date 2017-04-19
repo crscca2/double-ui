3502eb029e8b31366723da1e31a88b37a6e94e66
crscca2 <crscca2@yahoo.com>
2017-04-20 03:32:54 +0530

    witheureka

--- src/main/java/demo/UiApplication.java
+++ src/main/java/demo/UiApplication.java
@@ -5,17 +5,24 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
+
+@EnableDiscoveryClient
 public class UiApplication {
 
 	@RequestMapping("/user")
